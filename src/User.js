a51b6404ec52d4c24a71135b2668fe493fd31bd5
lida <lidalidalidalida>
2019-02-25 17:48:23 +0800

    加入React Navigation，link的时候settings.gradle中的路径中'\'需改为'/'

---
+++ src/User.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+type Props = {};
+export default class Main extends Component<Props> {
+  static navigationOptions = {
+    title: 'User'
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});

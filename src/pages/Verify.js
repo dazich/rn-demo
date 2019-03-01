b787b2c5fc6412a2827d80c296faf00470b19e33
lida <lidalidalidalida>
2019-03-01 19:33:44 +0800

    认证页

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -1,44 +1,53 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, TouchableOpacity, Linking } from 'react-native';
-import {
-	Toast,
-} from '@ant-design/react-native';
+import { Toast, Flex } from '@ant-design/react-native';
 import { createFetch, to, login } from '../utils';
+import { THEME } from "../config";
+import HeaderTitle from "../components/HeaderTitle";
 
-export default class Verify extends Component {
+const verifyStatus = {
+	1: '已认证',
+	3: '已过期',
+};
+
+export default class User extends Component {
 	static navigationOptions = {
-		title: 'Verify'
+		headerTitle: (
+			<HeaderTitle title="认证中心"/>
+		),
 	}
-
+	
 	constructor(props) {
 		super(props);
 		this.state = {
 			list: [],
 			user_info: {},
 		};
-
+		
 		try { this.query() } catch {}
 	}
-
+	
 	async query() {
-		const url = '/api/user/center';
+		const url = '/api/user/profile';
 		const [err, res] = await to(createFetch(url).then(r => r.data));
-		console.warn(res)
 		if (err || +res.code !== 0) {
 			Toast.fail((err && err.message) || res.message || '网络异常');
 			return;
 		}
-
-		const { list, user_info } = res.data;
-		this.setState({ list, user_info });
+		
+		const { list = [] } = res.data;
+		const [list0={}, list1={}] = list;
+		const verifyList = list0.list.concat(list1.list)
+		
+		this.setState({ list: verifyList });
 	}
-
-	_onPressItem = (url) => {
-		Linking.openURL(url).catch(err => Toast.fail(err));
+	
+	_onPressItem = (uri) => {
+		this.props.navigation.push('Web', {uri});
 	}
-
-
+	
+	
 	render() {
 		let { list, user_info } = this.state;
 		return (
@@ -46,24 +55,22 @@ export default class Verify extends Component {
 				<View style={styles.header}>
 					<View style={styles.user_info}>
 						<View style={styles.portrait}>
-							<Image source={user_info.user_photo} alt="" />
+							<Image source={{uri: user_info.user_photo}} alt="" />
 						</View>
-						<Text
-							style={`${user_info.user_phone ? styles.user_phone : styles.login_btn}`}
-							onPress={() => {
-								!user_info.user_phone && login();
-							}}
-						>
-							{user_info.user_phone || '立即登录'}
+						<Text>
+							认证无碍，提现畅快！
 						</Text>
 					</View>
 				</View>
 				{list.map((v, i) => (
 					<TouchableOpacity onPress={() => this._onPressItem(v.url)} key={i}>
-						<View style={{...styles.row, marginTop: v.margin_top}} >
-							<Image source={{uri: v.icon}} style={{width: 40, height: 40}} />
-              <Text style={styles.title}>{v.title}</Text>
-						</View>
+						<Flex justify="between" style={styles.row}>
+							<View style={{...styles.row, marginTop: v.margin_top}} >
+								<Image source={{uri: v.icon}} style={{width: 40, height: 40}} />
+								<Text style={styles.title}>{v.name}</Text>
+							</View>
+							<Text style={{...styles.verifyBtn, backgroundColor: +v.status === 1 ? THEME.COLOR : '#aaa'}}>{verifyStatus[v.status] || '去认证'}</Text>
+						</Flex>
 					</TouchableOpacity>
 				))}
 			</View>
@@ -89,14 +96,21 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		alignItems: 'center',
 		height: 80,
-		paddingLeft: 30,
+		paddingLeft: 10,
+		paddingRight: 10,
 		backgroundColor: '#fff',
-    borderBottomColor: '#f2f2f2',
-    borderBottomWidth: 1,
+		borderBottomColor: '#f2f2f2',
+		borderBottomWidth: 1,
 	},
 	title: {
-	  marginLeft: 20,
-    color: '#333',
-    fontSize: 20
-  }
+		marginLeft: 20,
+		color: '#333',
+		fontSize: 20
+	},
+	verifyBtn: {
+		padding: 10,
+		borderRadius: 4,
+		color: '#fff',
+		fontSize: 16,
+	}
 });

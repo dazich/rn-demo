1d5520bdca1c97fd38bae9924f4bccdd7b977d6a
dazich <497688626@qq.com>
2019-03-03 18:16:30 +0800

    slider、轮播

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
-import {
-	Toast,
-	Button
-} from '@ant-design/react-native';
+import {Slider, Button, Carousel} from '@ant-design/react-native';
 import HeaderTitle from "../components/HeaderTitle";
 import {APP_NAME} from "../config";
 
@@ -26,13 +23,51 @@ export default class Home extends Component {
 		};
 	};
 
+	state = {};
+
 	_gotoWeb = () => {
 		this.props.navigation.navigate('Web', {uri: 'http://120.78.170.195:5000/order'});
 	}
+
+	_onLoanNumChange = (e) => {
+		this.setState({loanNum: e});
+	}
+
 	render() {
+		const { min=500, max=1000, loanNum, disabled } = this.state;
 		return (
 			<ScrollView style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
+				<Text>{loanNum}</Text>
+				<Slider
+					min={min}
+					max={max}
+					value={loanNum}
+					defaultValue={max}
+					step={50}
+					onChange={this._onLoanNumChange}
+					disabled={disabled}
+				/>
+				<Carousel
+					style={styles.wrapper}
+					selectedIndex={1}
+					autoplay
+					infinite
+					afterChange={this.onHorizontalSelectedIndexChange}
+				>
+					<View
+						style={[styles.containerHorizontal, { backgroundColor: 'red' }]}
+					>
+						<Text>Carousel 1</Text>
+					</View>
+					<View
+						style={[
+							styles.containerHorizontal,
+							{ backgroundColor: 'yellow' },
+						]}
+					>
+						<Text>Carousel 2</Text>
+					</View>
+				</Carousel>
 				<Text style={styles.instructions}>To get started, edit App.js</Text>
 				<Text style={styles.instructions}>{instructions}</Text>
 				<Button
@@ -49,6 +84,12 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#F5FCFF',
+	},
+	containerHorizontal: {
+		flexGrow: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: 150,
 	},
 	welcome: {
 		fontSize: 20,
